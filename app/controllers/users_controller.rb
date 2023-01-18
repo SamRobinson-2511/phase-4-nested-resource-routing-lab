d9cfb214bc1204f15bd5d155d5f0dc7c8f3a425b
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2023-01-17 23:39:21 -0600

    commit tree e48def244cbc47b27fee13cf9b62af2ceae7e9a3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
 
+  def index 
+    
+  end
+
+
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find(params[:id])
     render json: user, include: :items
   end
 
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:username, :city)
+  end
+
+  def find_user
+    User.find(params[:id])
+  end
 end
